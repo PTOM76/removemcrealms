45c663fa59ccf7e6705556ec776d744550af2839
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2025-05-13 21:53:26 +0900

    port to 1.21.4

--- common/src/main/java/net/pitan76/removemcrealms/mixin/TitleScreenMixin.java
+++ common/src/main/java/net/pitan76/removemcrealms/mixin/TitleScreenMixin.java
@@ -6,17 +6,18 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
 
     @Inject(method = "createNormalMenuOptions", at = @At("HEAD"))
-    private void createNormalMenuOptions(CallbackInfo info) {
+    private void createNormalMenuOptions(int y, int rowHeight, CallbackInfoReturnable<Integer> cir) {
         CommonClass.mixiningScreen = true;
     }
 
     @Inject(method = "createNormalMenuOptions", at = @At("RETURN"))
-    private void createNormalMenuOptionsReturn(CallbackInfo info) {
+    private void createNormalMenuOptionsReturn(int y, int rowHeight, CallbackInfoReturnable<Integer> cir) {
         CommonClass.mixiningScreen = false;
     }
 }
